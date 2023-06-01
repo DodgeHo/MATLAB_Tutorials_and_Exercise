5344b9acaf92b71ed6c16d3f8a71bc367f080424
Dodge (Lang HE) <asdsay@gmail.com>
2023-06-01 22:45:06 +0800

    English version

--- matlab_exercises/kurtosis.m
+++ matlab_exercises/kurtosis.m
@@ -1,6 +1,7 @@
+%%
 clear all;clc;clf;close all
 
-%% 生成信号
+%% Signal synthesis
 fs=1000;
 SampFreq = fs;
 t=0:1/fs:1;
@@ -9,21 +10,21 @@ f0=100;
 x=A*exp(1j*(2*pi*(f0+1/2*150*(t.^2))));
 
 figure(1)
-plot(t,real(x))  % 画的是实部的时域波形
-title('时域波形图')
+plot(t,real(x))  % draw the time waveform of the real part
+title('Time-domain waveform')
 
-%% 频谱
+%% frequency spectrum
 Spec = 2*abs(fft(real(x)))/length(x);
 Spec = Spec(1:round(end/2));
 Freqbin = linspace(0,SampFreq/2,length(Spec));
 
 figure(2)
 plot(Freqbin,Spec,'linewidth',2);
-xlabel('\fontname{宋体}频率\fontname{Times New Roman}/Hz','FontSize',12);
-ylabel('\fontname{宋体}幅值\fontname{Times New Roman}/(m^2 \cdot t^{-1})','FontSize',12);
+xlabel('\FFrequency\fontname{Times New Roman}/Hz','FontSize',12);
+ylabel('\Amplitude\fontname{Times New Roman}/(m^2 \cdot t^{-1})','FontSize',12);
 % axis([0 500 0 1.2*max(Spec)])
-%% 谱峭度
 
+%% Spectral kurtosis
 bw=20;
 %bw=3*f0;
 step=1;
@@ -35,7 +36,7 @@ kurtosisArray = zeros(1, length(cf));
 
 for idx = 1:length(cf)
     i = cf(idx)-bw/2 : cf(idx)+bw/2;
-    i(i<1) = []; i(i>fs/2) = [];  % 超出范围的值删除
+    i(i<1) = []; i(i>fs/2) = [];  % Values out of range are deleted
     if isempty(i)
         continue;
     end
